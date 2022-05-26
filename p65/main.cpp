65e68034824e527f71e17d4f5e3bb10ce7f8df6f
GunwooYun <gunwoo87@gmail.com>
2022-05-26 16:12:33 +0900

    input string exam

--- p65/main.cpp
+++ p65/main.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-	cout << "Enter width >>";
+	cout << "Enter width >> ";
 
 	int width;
 	cin >> width;
 
-	cout << "Enter height >>";
+	cout << "Enter height >> ";
 	int height;
 	cin >> height;
 
+	cout << "String test >> ";
+	string str;
+	cin >> str;
+
+
+
 	int area = width * height;
 	cout << "area is " << area << endl;
+	cout << str;
 	return 0;
 }
