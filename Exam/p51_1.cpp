006b13e6b8b0d7b653abeda00dcedf42008d4415
GunwooYun <gunwoo87@gmail.com>
2022-05-26 17:33:58 +0900

    book exam ch1

---
+++ Exam/p51_1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+//using namespace std;
+
+int main()
+{
+  std::cout << "My name is Mike.\n";
+  return 0;
+}
---
+++ Exam/p52_2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "computer engineering\n";
+    cout << "21 years old" << endl << "president" << endl;
+    return 0;
+}
