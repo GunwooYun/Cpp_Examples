ea11e4a0803ea0d0a686f68518b4b249f0b6c40d
GunwooYun <gunwoo87@gmail.com>
2022-05-27 11:58:14 +0900

    Add some comments

--- CH03/p104/p104_main.cpp
+++ CH03/p104/p104_main.cpp
@@ -7,26 +7,46 @@ public:
 	double getArea(); // Declare method
 };
 
+class _Circle {
+	int radius; // Declare variable
+public:	
+	double getArea(); // Declare method
+	void setRadius(int r);
+};
+
 double Circle::getArea() {
 	return 3.14 * radius * radius; // 멤버변수는 매개변수로 안 넣고 바로 사용 가능
+}
+
+double _Circle::getArea() {
+	return 3.14 * radius * radius; // 멤버변수는 매개변수로 안 넣고 바로 사용 가능
+}
+
+void _Circle::setRadius(int r) {
+	radius = r;
 }
 
 
 
 int main(void) {
 	Circle donut; // Get instance
-	donut.radius = 1;
 
+	// Able to access variable and function, becase they are public
+	donut.radius = 1;
 	double area = donut.getArea();
 
 	cout << "donut area is " << area << endl;
 
-	Circle pizza;
-	pizza.radius = 30;
+	Circle pizza; // Get another instance
 
+	pizza.radius = 30;
 	area = pizza.getArea();
 
 	cout << "pizza area is " << area << endl;
+	cout << "pizza size is " << sizeof(pizza) << endl;
+	_Circle c1;
+	c1.setRadius(10);
+	cout << "c1 area is " << c1.getArea() << endl;
 
 	return 0;
 }
