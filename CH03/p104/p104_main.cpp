c26d9fb6cc089a9a1ffe17070b2fc8446df0e603
GunwooYun <gunwoo87@gmail.com>
2022-05-27 12:47:08 +0900

    delegating constructor

--- CH03/p104/p104_main.cpp
+++ CH03/p104/p104_main.cpp
@@ -2,51 +2,59 @@
 using namespace std;
 
 class Circle {
-public:
+private:
 	int radius; // Declare variable
+public:
+	Circle(); // basic constructor
+	Circle(int r); // 1 parameter constructor
 	double getArea(); // Declare method
+	double getRadius();
 };
 
-class _Circle {
-	int radius; // Declare variable
-public:	
-	double getArea(); // Declare method
-	void setRadius(int r);
-};
+Circle::Circle() {
+	Circle(1);
+	//radius = 1;
+	//cout << "radius : " << radius << endl;
+}
+
+Circle::Circle(int r) {
+	radius = r;
+	cout << "radius : " << radius << endl;
+}
 
 double Circle::getArea() {
-	return 3.14 * radius * radius; // 멤버변수는 매개변수로 안 넣고 바로 사용 가능
+	return 3.14 * (double)radius * (double)radius; // 멤버변수는 매개변수로 안 넣고 바로 사용 가능
 }
 
-double _Circle::getArea() {
-	return 3.14 * radius * radius; // 멤버변수는 매개변수로 안 넣고 바로 사용 가능
+double Circle::getRadius() {
+	return radius;
 }
-
-void _Circle::setRadius(int r) {
-	radius = r;
-}
-
-
 
 int main(void) {
 	Circle donut; // Get instance
+	cout << "test " << donut.getRadius() << endl;
 
 	// Able to access variable and function, becase they are public
-	donut.radius = 1;
+	//donut.radius = 1;
 	double area = donut.getArea();
 
 	cout << "donut area is " << area << endl;
 
 	Circle pizza; // Get another instance
 
-	pizza.radius = 30;
+	//pizza.radius = 30;
 	area = pizza.getArea();
 
 	cout << "pizza area is " << area << endl;
 	cout << "pizza size is " << sizeof(pizza) << endl;
-	_Circle c1;
-	c1.setRadius(10);
-	cout << "c1 area is " << c1.getArea() << endl;
+
+	Circle one;
+	area = one.getArea();
+	cout << "one area is " << area << endl;
+
+	Circle round(10);
+	area = round.getArea();
+	cout << "round area is " << area << endl;
 
 	return 0;
 }
