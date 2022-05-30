fe744bace4eb0a77d502c08a9d99ecaef31dcce6
GunwooYun <gunwoo87@gmail.com>
2022-05-30 15:04:33 +0900

    String class

---
+++ CH04/p192/p192_main.cpp
@@ -0,0 +1,24 @@
+/* Page 192. Exam 4-11. String class */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void) {
+
+	string str; // Declare string instance
+	string address("¼­¿ï½Ã ¼ººÏ±¸ »ï¼±µ¿ 389");
+	string copyAddress(address); // copy string address to copyAddress
+
+	char txt[] = { 'L', 'O', 'V', 'E', '\0' };
+	string title(txt);
+
+	// print string instance
+
+	cout << str << endl; // NULL, show nothing
+	cout << address << endl; // show string
+	cout << copyAddress << endl; // show copied string
+	cout << title << endl; // show string of array
+
+	return 0;
+}
