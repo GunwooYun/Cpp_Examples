eeb1cb441c5848a50772c97594ddb9f1db0270ca
GunwooYun <gunwoo87@gmail.com>
2022-05-30 15:47:56 +0900

    swap using call by reference

---
+++ CH05/p236/p236_main.cpp
@@ -0,0 +1,23 @@
+/* Page 236. Pic 5-8. Swap using call by reference */
+
+#include <iostream>
+
+using namespace std;
+
+/* Parameters are reference variable */
+void swap(int& a, int& b) {
+	int tmp;
+
+	tmp = a;
+	a = b;
+	b = tmp;
+}
+
+int main(void) {
+
+	int m = 2, n = 9;
+	swap(m, n);
+	cout << "m : " << m << " n : " << n << endl; // m : 9 n : 2
+
+	return 0;
+}
