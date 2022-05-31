1d2eadae1cc9045602a5b164494a5266de67b618
GunwooYun <gunwoo87@gmail.com>
2022-05-31 16:51:10 +0900

    default parameter

---
+++ CH06/p289/p289_main.cpp
@@ -0,0 +1,16 @@
+/* Page 289. Exam 6-5. Overloading */
+#include <iostream>
+using namespace std;
+
+void fillLine(int n = 25, char c = '*') {
+	for (int i = 0; i < n; i++) {
+		cout << c;
+	}
+	cout << endl;
+}
+
+int main() {
+	fillLine();
+	fillLine(10, '@');
+	return 0;
+}
