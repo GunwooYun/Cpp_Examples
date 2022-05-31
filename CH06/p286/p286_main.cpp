0fb5e519cc2f04ca1abcb7cd905a9c9fbbe58924
GunwooYun <gunwoo87@gmail.com>
2022-05-31 16:49:17 +0900

    default parameter

---
+++ CH06/p286/p286_main.cpp
@@ -0,0 +1,29 @@
+/* Page 286. Exam 6-3. Overloading */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void star(int a = 5);
+void msg(int id, string text = "");
+
+void star(int a) {
+	for (int i = 0; i < a; i++) cout << '*';
+	cout << endl;
+}
+
+void msg(int id, string text) {
+	cout << id << ' ' << text << endl;
+}
+
+
+int main() {
+	star(); // start(5)
+	star(10);
+
+	msg(10); // msg(10, "")
+	msg(10, "Hello");
+
+	return 0;
+
+}
