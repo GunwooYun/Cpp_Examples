48532bdf7684ee63c1396cf0be04ad7c9079e37e
GunwooYun <gunwoo87@gmail.com>
2022-06-03 16:40:51 +0900

    CH09

---
+++ CH09/p431/p431_main.cpp
@@ -0,0 +1,34 @@
+/* Page 431. Exam 9-2. Virtual function */
+#include <iostream>
+
+using namespace std;
+
+class Base {
+public:
+	virtual void f() {
+		cout << "Base::f() called" << endl;
+	}
+};
+
+class Derived :public Base {
+public:
+	virtual void f() {
+		cout << "Derived::f() called" << endl;
+	}
+};
+
+int main() {
+	Derived d, * pDer;
+	pDer = &d;
+	pDer->f();
+
+	Base* pBase;
+	pBase = pDer; // upcasting
+	pBase->f();
+
+	return 0;
+}
+
+/*
+Circle::draw() called
+*/
