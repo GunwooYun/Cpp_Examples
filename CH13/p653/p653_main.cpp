22dca00b33de86d912ea2d6faced7a03041de910
GunwooYun <gunwoo87@gmail.com>
2022-06-03 11:57:59 +0900

    Page 653. Exam 13-4. exception

---
+++ CH13/p653/p653_main.cpp
@@ -0,0 +1,32 @@
+/* Page 653. Exam 13-4. exception */
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, sum, average;
+	while (true) {
+		cout << "합을 입력하세요>>";
+		cin >> sum;
+		cout << "인원수를 입력하세요>>";
+		cin >> n;
+		try {
+			if (n <= 0)	 // 오류 탐지
+				throw n; // 예외 발생. catch(int x) 블록으로 점프				
+			else
+				average = sum / n;
+		}
+		catch (int x) {
+			cout << "예외 발생!! " << x << "으로 나눌 수 없음" << endl;
+			average = 0;
+			cout << endl;
+			continue;
+		}
+		cout << "평균 = " << average << endl << endl; // 평균 출력
+	}
+}
+
+/*
+합을 입력하세요>>15
+인원수를 입력하세요>>0
+예외 발생!! 0으로 나눌 수 없음
+*/
